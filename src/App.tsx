65b1900e02321f1cf71e789978576b68faa4ee89
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2025-11-16 19:18:16 +0330

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import Cities from "./pages/Cities";
+import Map from "./pages/Map";
+import Calendar from "./pages/Calendar";
+import Settings from "./pages/Settings";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/cities", element: <Cities /> },
+  { path: "/map", element: <Map /> },
+  { path: "/calendar", element: <Calendar /> },
+  { path: "/settings", element: <Settings /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
