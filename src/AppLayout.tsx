80347db648ab97e5821bdd7b6c942f3c6a7f5226
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2025-11-19 19:32:49 +0330

    edit layout

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./features/navbar/Navbar";
+import MainHeader from "./components/MainHeader";
 
 export default function AppLayout() {
   return (
     <div className="bg-linear-to-r from-violet-400 to-violet-300/85 h-screen overflow-hidden">
       <div className="flex gap-10 h-full 2xl:max-w-7xl 2xl:mx-auto">
         <Navbar />
-        <div className="h-full w-full">
+        <div className="h-full w-full flex flex-col gap-8">
+          <MainHeader />
           <Outlet />
         </div>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,13 @@
 import stormy from "../assets/stormy.png";
-import MainHeader from "../components/MainHeader";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <MainHeader />
-
-      <div className="blur-[2px] mt-8 w-full">
-        <img
-          src={stormy}
-          alt="Background"
-          className="w-[99%] rounded-4xl shadow"
-        />
-      </div>
+    <div className="blur-[2px] w-full">
+      <img
+        src={stormy}
+        alt="Background"
+        className="w-[99%] rounded-4xl shadow"
+      />
     </div>
   );
 }
