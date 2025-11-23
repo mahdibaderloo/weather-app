c0a73dd85d044e7d2259a26f72383dffdefcb146
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2025-11-23 19:24:15 +0330

    add store

---
+++ src/store/LocationStore.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+interface LocationState {
+  lat: number;
+  lon: number;
+  city?: string;
+  setLocation: (lat: number, lon: number) => void;
+  setCity: (city: string) => void;
+}
+
+export const useLocationStore = create<LocationState>((set) => ({
+  lat: 35,6892,
+  lon: 51.3890,
+  city: "Tehran",
+  setLocation: (lat, lon) => set({lat, lon}),
+  setCity: (city) => set({city})
+}));
