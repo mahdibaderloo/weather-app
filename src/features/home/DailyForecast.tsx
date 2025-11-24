360a532f75d9cb907f1d010487fdfc2da5ad5a59
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2025-11-24 11:45:46 +0330

    DailyForecast component

---
+++ src/features/home/DailyForecast.tsx
@@ -0,0 +1,42 @@
+import rainyIcon from "../../assets/rainy.svg";
+
+export default function DailyForecast() {
+  return (
+    <section className="relative z-50 p-4 bg-linear-to-r from-violet-400/35 to-violet-800/60 rounded-4xl w-[49%] h-fit flex flex-col">
+      <p className="font-semibold text-xl text-violet-200">3-Day Forecast</p>
+
+      <ul className="mt-6 divide-y-2 divide-violet-950 mb-7">
+        <li className="flex items-center justify-between text-violet-300 text-xl font-semibold py-4">
+          <p className="w-20">Today</p>
+          <div className="flex items-center">
+            <img src={rainyIcon} alt="icon" className="w-14" />
+            <span>Rainy</span>
+          </div>
+          <p>
+            20 / <span className="text-violet-400">9</span>
+          </p>
+        </li>
+        <li className="flex items-center justify-between text-violet-300 text-xl font-semibold py-4">
+          <p className="w-20">Mon</p>
+          <div className="flex items-center">
+            <img src={rainyIcon} alt="icon" className="w-14" />
+            <span>Rainy</span>
+          </div>
+          <p>
+            20 / <span className="text-violet-400">9</span>
+          </p>
+        </li>
+        <li className="flex items-center justify-between text-violet-300 text-xl font-semibold py-4">
+          <p className="w-20">Tue</p>
+          <div className="flex items-center">
+            <img src={rainyIcon} alt="icon" className="w-14" />
+            <span>Rainy</span>
+          </div>
+          <p>
+            20 / <span className="text-violet-400">9</span>
+          </p>
+        </li>
+      </ul>
+    </section>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import rainy from "../assets/rainy.png";
 import rainyIcon from "../assets/rainy.svg";
+import DailyForecast from "../features/home/DailyForecast";
 import InformationSection from "../features/home/InformationSection";
 import { useWeather } from "../hooks/useWeather";
 import { useLocationStore } from "../store/LocationStore";
@@ -27,44 +28,8 @@ export default function Home() {
           max={Math.ceil(data?.daily.temperature_2m_max[0])}
           min={Math.ceil(data?.daily.temperature_2m_min[0])}
         />
-        <section className="relative z-50 p-4 bg-linear-to-r from-violet-400/35 to-violet-800/60 rounded-4xl w-[49%] h-fit flex flex-col">
-          <p className="font-semibold text-xl text-violet-200">
-            3-Day Forecast
-          </p>
 
-          <ul className="mt-6 divide-y-2 divide-violet-950 mb-7">
-            <li className="flex items-center justify-between text-violet-300 text-xl font-semibold py-4">
-              <p className="w-20">Today</p>
-              <div className="flex items-center">
-                <img src={rainyIcon} alt="icon" className="w-14" />
-                <span>Rainy</span>
-              </div>
-              <p>
-                20 / <span className="text-violet-400">9</span>
-              </p>
-            </li>
-            <li className="flex items-center justify-between text-violet-300 text-xl font-semibold py-4">
-              <p className="w-20">Mon</p>
-              <div className="flex items-center">
-                <img src={rainyIcon} alt="icon" className="w-14" />
-                <span>Rainy</span>
-              </div>
-              <p>
-                20 / <span className="text-violet-400">9</span>
-              </p>
-            </li>
-            <li className="flex items-center justify-between text-violet-300 text-xl font-semibold py-4">
-              <p className="w-20">Tue</p>
-              <div className="flex items-center">
-                <img src={rainyIcon} alt="icon" className="w-14" />
-                <span>Rainy</span>
-              </div>
-              <p>
-                20 / <span className="text-violet-400">9</span>
-              </p>
-            </li>
-          </ul>
-        </section>
+        <DailyForecast />
       </div>
 
       <section className="relative z-50 p-4 bg-linear-to-b from-violet-400/35 to-violet-800/60 rounded-4xl w-full h-fit flex flex-col gap-6 justify-between mt-6">
