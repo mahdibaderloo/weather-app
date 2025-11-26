37feaf44a780fdc5dd0f46707d91967d59246bdf
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2025-11-26 23:32:26 +0330

    save cities in local storage

--- src/store/LocationStore.ts
+++ src/store/LocationStore.ts
@@ -1,17 +1,34 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface LocationState {
   lat: number;
   lon: number;
   city?: string;
+  cityList: string[];
   setLocation: (lat: number, lon: number) => void;
   setCity: (city: string) => void;
+  addCity: (city: string) => void;
 }
 
-export const useLocationStore = create<LocationState>((set) => ({
-  lat: 35.6892,
-  lon: 51.389,
-  city: "Tehran",
-  setLocation: (lat, lon) => set({ lat, lon }),
-  setCity: (city) => set({ city }),
-}));
+export const useLocationStore = create<LocationState>()(
+  persist(
+    (set) => ({
+      lat: 35.6892,
+      lon: 51.389,
+      city: "Tehran",
+      cityList: [],
+
+      setLocation: (lat, lon) => set({ lat, lon }),
+      setCity: (city) => set({ city }),
+
+      addCity: (city) =>
+        set((state) => ({
+          cityList: [...state.cityList, city],
+        })),
+    }),
+    {
+      name: "cities",
+    }
+  )
+);
