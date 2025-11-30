216cc39b23d77c0c9e54390e62ccfb441d21022c
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2025-11-30 19:34:53 +0330

    complete help for AQI

--- src/pages/AirQualityIndex.tsx
+++ src/pages/AirQualityIndex.tsx
@@ -35,7 +35,7 @@ export default function AirQualityIndex() {
         </div>
       </div>
 
-      <div className="mt-16 flex justify-center">
+      <div className="mt-16 flex flex-col justify-center items-center">
         <ul className="w-[85%] grid grid-cols-6 rounded-3xl overflow-hidden">
           <li className="bg-green-800 flex justify-center items-center text-white text-lg font-medium px-2 py-1">
             Clean
@@ -55,6 +55,16 @@ export default function AirQualityIndex() {
           <li className="bg-red-900 flex justify-center items-center text-white text-lg font-medium px-2 py-1">
             Dangerous
           </li>
+        </ul>
+
+        <ul className="w-[85%] grid grid-cols-6 mt-4">
+          <li className="text-violet-200 text-lg font-medium">0</li>
+          <li className="text-violet-200 text-lg font-medium">50</li>
+          <li className="text-violet-200 text-lg font-medium">100</li>
+          <li className="text-violet-200 text-lg font-medium">150</li>
+          <li className="text-violet-200 text-lg font-medium">200</li>
+          <li className="text-violet-200 text-lg font-medium">+300</li>
+          {/* <li className="text-white text-lg font-medium px-3 py-1">+</li> */}
         </ul>
       </div>
     </div>
