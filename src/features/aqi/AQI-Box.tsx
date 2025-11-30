24d899c3ac96359fa9895d3746386cb5b64aae6b
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2025-11-30 19:51:56 +0330

    add AQI-Box component

---
+++ src/features/aqi/AQI-Box.tsx
@@ -0,0 +1,19 @@
+import { getFormattedDate } from "../../utils/date";
+
+export default function AQIBox() {
+  return (
+    <div className="w-[35%] flex flex-col bg-linear-to-b from-yellow-400 to-violet-400/45 text-violet-50 rounded-4xl p-8">
+      <p className="font-semibold text-2xl pb-4 border-b-2 border-white/20">
+        Air quality index now
+      </p>
+      <p className="mt-4 mx-auto text-7xl font-bold">491</p>
+      <p className="text-lg font-medium mt-4 text-white/80">Dangerous</p>
+      <p className="text-lg font-medium mt-1 text-white/80">
+        Pollutant index: 10 PM
+      </p>
+      <p className="text-lg font-medium mt-1 text-white/80">
+        {getFormattedDate(new Date())} - 12:40
+      </p>
+    </div>
+  );
+}
--- src/pages/AirQualityIndex.tsx
+++ src/pages/AirQualityIndex.tsx
@@ -1,5 +1,5 @@
+import AQIBox from "../features/aqi/AQI-Box";
 import Help from "../features/aqi/Help";
-import { getFormattedDate } from "../utils/date";
 
 export default function AirQualityIndex() {
   return (
@@ -7,33 +7,8 @@ export default function AirQualityIndex() {
       <h3 className="font-bold text-4xl text-violet-950">Air Quality Index</h3>
 
       <div className="w-full flex gap-4 mt-8 justify-center">
-        <div className="w-[35%] flex flex-col bg-linear-to-b from-yellow-400 to-violet-400/45 text-violet-50 rounded-4xl p-8">
-          <p className="font-semibold text-2xl pb-4 border-b-2 border-white/20">
-            Air quality index now
-          </p>
-          <p className="mt-4 mx-auto text-7xl font-bold">491</p>
-          <p className="text-lg font-medium mt-4 text-white/80">Dangerous</p>
-          <p className="text-lg font-medium mt-1 text-white/80">
-            Pollutant index: 10 PM
-          </p>
-          <p className="text-lg font-medium mt-1 text-white/80">
-            {getFormattedDate(new Date())} - 12:40
-          </p>
-        </div>
-
-        <div className="w-[35%] flex flex-col bg-linear-to-b from-red-900/90 to-violet-400/45 text-violet-50 rounded-4xl p-8">
-          <p className="font-semibold text-2xl pb-4 border-b-2 border-white/20">
-            Index of the last 24 hours
-          </p>
-          <p className="mt-4 mx-auto text-7xl font-bold">491</p>
-          <p className="text-lg font-medium mt-4 text-white/80">Dangerous</p>
-          <p className="text-lg font-medium mt-1 text-white/80">
-            Pollutant index: 10 PM
-          </p>
-          <p className="text-lg font-medium mt-1 text-white/80">
-            {getFormattedDate(new Date())} - 12:40
-          </p>
-        </div>
+        <AQIBox />
+        <AQIBox />
       </div>
 
       <Help />
