2375209e7e250fc75b29195a54b6caeb430571b8
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2025-12-02 15:45:13 +0330

    dark theme for Navbar

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,34 +1,57 @@
+import { useThemeStore } from "../../store/themeStore";
+
 import logoImage from "../../assets/weather-app.png";
 // import calendarImage from "../../assets/calendar.svg";
+// import calendarDark from "../../assets/calendar-dark.svg";
 import listImage from "../../assets/list.svg";
+import listDark from "../../assets/list-dark.svg";
 import mapImage from "../../assets/map.svg";
-import settingsImage from "../../assets/settings.svg";
+import mapDark from "../../assets/map-dark.svg";
 import maskImage from "../../assets/mask.svg";
+import maskDark from "../../assets/mask-dark.svg";
+import settingsImage from "../../assets/settings.svg";
+import settingsDark from "../../assets/settings-dark.svg";
+
 import NavbarItem from "./NavbarItem";
 
 interface Items {
   id: number;
   url: string;
   image: string;
+  dark: string;
   title: string;
 }
 
 const items: Items[] = [
-  { id: 1, url: "/", image: logoImage, title: "Home" },
-  { id: 2, url: "/cities", image: listImage, title: "Cities" },
-  { id: 3, url: "/map", image: mapImage, title: "Map" },
-  { id: 4, url: "/air-quality", image: maskImage, title: "AQI" },
-  // { id: 5, url: "/calendar", image: calendarImage, title: "Calendar" },
-  { id: 6, url: "/settings", image: settingsImage, title: "Settings" },
+  { id: 1, url: "/", image: logoImage, dark: logoImage, title: "Home" },
+  { id: 2, url: "/cities", image: listImage, dark: listDark, title: "Cities" },
+  { id: 3, url: "/map", image: mapImage, dark: mapDark, title: "Map" },
+  {
+    id: 4,
+    url: "/air-quality",
+    image: maskImage,
+    dark: maskDark,
+    title: "AQI",
+  },
+  // { id: 5, url: "/calendar", image: calendarImage, dark: calendarDark, title: "Calendar" },
+  {
+    id: 6,
+    url: "/settings",
+    image: settingsImage,
+    dark: settingsDark,
+    title: "Settings",
+  },
 ];
 
 export default function Navbar() {
+  const { theme } = useThemeStore();
+
   return (
-    <nav className="w-22 h-[852px] bg-linear-to-r from-violet-900/75 to-violet-500/50 px-4 rounded-full mt-8 shadow">
+    <nav className="w-22 h-[852px] bg-linear-to-r from-violet-900/75 to-violet-500/50 dark:from-slate-900 dark:to-violet-950/60 px-4 rounded-full mt-8 shadow">
       <div className="blur-2xl" />
       <ul className="w-full h-full flex flex-col gap-8 pt-2">
         {items.map((item) => (
-          <NavbarItem key={item.id} item={item} />
+          <NavbarItem key={item.id} item={item} theme={theme} />
         ))}
       </ul>
     </nav>
--- src/features/navbar/NavbarItem.tsx
+++ src/features/navbar/NavbarItem.tsx
@@ -5,12 +5,14 @@ type NavbarItemProp = {
     id: number;
     url: string;
     image: string;
+    dark: string;
     title: string;
   };
+  theme: "dark" | "light";
 };
 
-export default function NavbarItem({ item }: NavbarItemProp) {
-  const { url, image, title } = item;
+export default function NavbarItem({ item, theme }: NavbarItemProp) {
+  const { url, image, title, dark } = item;
 
   return (
     <li className="flex flex-col items-center justify-center">
@@ -18,13 +20,15 @@ export default function NavbarItem({ item }: NavbarItemProp) {
         to={url}
         className={({ isActive }) =>
           isActive
-            ? "bg-linear-to-r from-violet-500 to-violet-800 p-4 w-18 rounded-full shadow"
-            : "bg-linear-to-r from-violet-500 to-violet-300 p-4 w-18 rounded-full transition-all delay-150"
+            ? "bg-linear-to-r from-violet-500 to-violet-800 dark:bg-linear-to-b dark:from-slate-950 dark:to-violet-900 p-4 w-18 rounded-full shadow"
+            : "bg-linear-to-r from-violet-500 to-violet-300 dark:from-violet-700 dark:to-slate-900 p-4 w-18 rounded-full transition-all delay-150"
         }
       >
-        <img src={image} alt="Logo" />
+        <img src={theme === "dark" ? dark : image} alt="Logo" />
       </NavLink>
-      <span className="text-violet-950 font-medium mt-1">{title}</span>
+      <span className="text-violet-950 dark:text-violet-100 font-medium mt-1">
+        {title}
+      </span>
     </li>
   );
 }
