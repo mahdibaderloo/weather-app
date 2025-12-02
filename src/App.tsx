7ce150199ee9adb83d44123ae23a4a6e6c0dadcd
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2025-12-02 15:13:14 +0330

    hidden calendar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Cities from "./pages/Cities";
 import Map from "./pages/Map";
-import Calendar from "./pages/Calendar";
+// import Calendar from "./pages/Calendar";
 import Settings from "./pages/Settings";
 import AppLayout from "./AppLayout";
 import Profile from "./pages/Profile";
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
       { path: "/", element: <Home /> },
       { path: "/cities", element: <Cities /> },
       { path: "/map", element: <Map /> },
-      { path: "/calendar", element: <Calendar /> },
+      // { path: "/calendar", element: <Calendar /> },
       { path: "/air-quality", element: <AirQualityIndex /> },
       { path: "/settings", element: <Settings /> },
       { path: "/profile", element: <Profile /> },
--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import logoImage from "../../assets/weather-app.png";
-import calendarImage from "../../assets/calendar.svg";
+// import calendarImage from "../../assets/calendar.svg";
 import listImage from "../../assets/list.svg";
 import mapImage from "../../assets/map.svg";
 import settingsImage from "../../assets/settings.svg";
@@ -18,7 +18,7 @@ const items: Items[] = [
   { id: 2, url: "/cities", image: listImage, title: "Cities" },
   { id: 3, url: "/map", image: mapImage, title: "Map" },
   { id: 4, url: "/air-quality", image: maskImage, title: "AQI" },
-  { id: 5, url: "/calendar", image: calendarImage, title: "Calendar" },
+  // { id: 5, url: "/calendar", image: calendarImage, title: "Calendar" },
   { id: 6, url: "/settings", image: settingsImage, title: "Settings" },
 ];
 
