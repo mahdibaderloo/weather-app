fb2fcb005b06793b38c38f28058ef5fc67831127
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2025-12-09 20:57:37 +0330

    Map page for mobile size

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -18,22 +18,23 @@ export default function Map() {
 
   return (
     <>
-      <div className="flex justify-between items-center">
+      <div className="flex flex-col lg:flex-row gap-4 lg:gap-0 justify-between items-center">
         <button
           onClick={handleGetLocation}
-          className="bg-linear-to-r from-violet-900/90 to-violet-800/65 dark:from-violet-900/90 dark:to-slate-950/30 text-violet-100 text-lg font-medium w-fit px-8 py-3 rounded-4xl cursor-pointer shadow hover:bg-violet-950 transition-all"
+          className="w-full text-center bg-linear-to-r from-violet-900/90 to-violet-800/65 dark:from-violet-900/90 dark:to-slate-900/90 lg:from-violet-900/90 lg:to-violet-800/65 lg:dark:from-violet-900/90 lg:dark:to-slate-950/30 text-violet-100 lg:text-lg font-medium lg:w-fit px-8 py-3 rounded-4xl cursor-pointer shadow hover:bg-violet-950 transition-all"
         >
           Get My Location
         </button>
         <Link
           to="/"
-          className="bg-linear-to-r from-violet-900/90 to-violet-800/65 dark:from-slate-950/30 dark:to-violet-900/90 text-violet-100 text-lg font-medium w-fit px-8 py-3 rounded-4xl cursor-pointer shadow hover:bg-violet-950 transition-all"
+          className="w-full text-center bg-linear-to-r from-violet-900/90 to-violet-800/65 dark:from-slate-900/90 dark:to-violet-900/90 lg:from-violet-900/90 lg:to-violet-800/65 lg:dark:from-slate-950/30 lg:dark:to-violet-900/90 text-violet-100 lg:text-lg font-medium lg:w-fit px-8 py-3 rounded-4xl cursor-pointer shadow hover:bg-violet-950 transition-all"
         >
           Get Information About This City
         </Link>
       </div>
 
       <WeatherMap />
+      <div className="w-full h-30" />
     </>
   );
 }
