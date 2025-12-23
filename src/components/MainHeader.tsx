0a934d2029c419171861963a003e1b8273cbbdd2
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-12-23 18:28:58 +0330

    fix Header styles for tablet size

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -57,45 +57,45 @@ export default function MainHeader() {
   }
 
   return (
-    <header className="flex items-center justify-between w-full mt-8 lg:pr-8 lg:fixed lg:mx-22 lg:w-[90%] 2xl:static 2xl:w-full 2xl:mx-0">
+    <header className="flex items-center justify-between w-full mt-2 lg:pr-2 lg:fixed lg:mx-14 lg:w-[90%] 2xl:static 2xl:w-full 2xl:mx-0">
       <form
         onSubmit={handleSubmitSearch}
         className={`bg-linear-to-r ${
           error
             ? "from-red-200/85 to-red-900/85 dark:from-red-950/85 dark:to-red-900"
             : "from-violet-200/85 to-violet-900/85 dark:from-violet-950/85 dark:to-slate-900"
-        } flex items-center gap-2 w-full lg:w-[40%] 2xl:w-1/2 h-14 p-2 md:mx-8 2xl:mx-0 rounded-4xl shadow`}
+        } flex items-center gap-1 w-full lg:w-[50%] 2xl:w-1/2 h-10 p-2 md:mx-8 2xl:mx-0 rounded-4xl shadow`}
       >
         <img
           src={theme === "dark" ? searchIconDark : searchIcon}
           alt="Search icon"
-          className="w-12 lg:w-16"
+          className="w-12 lg:w-10"
         />
         <input
           type="text"
-          className="w-full h-full outline-none border-none text-violet-950 dark:text-violet-100 md:text-lg font-medium"
+          className="w-full h-full outline-none border-none text-violet-950 dark:text-violet-100 md:text-lg lg:text-sm font-medium"
           placeholder="Search city"
           value={error ? error : query}
           disabled={error.length !== 0}
           onChange={(e) => setQuery(e.target.value)}
         />
       </form>
-      <div className="hidden lg:flex items-center gap-4">
+      <div className="hidden lg:flex items-center gap-2">
         <div
-          className="w-14 h-14 flex items-center justify-center bg-linear-to-l from-violet-500/85 to-violet-900/95 dark:from-violet-950 dark:to-slate-900 rounded-full cursor-pointer shadow"
+          className="w-10 h-10 flex items-center justify-center bg-linear-to-l from-violet-500/85 to-violet-900/95 dark:from-violet-950 dark:to-slate-900 rounded-full cursor-pointer shadow"
           onClick={handleToggleTheme}
         >
           <img
             src={theme === "light" ? lightIcon : darkIcon}
             alt="Theme icon"
-            className="w-9"
+            className="w-6"
           />
         </div>
         <Link
           to="/profile"
-          className="w-14 h-14 flex items-center justify-center bg-linear-to-r from-violet-500/85 to-violet-900/95 dark:from-violet-950 dark:to-slate-900 rounded-full cursor-pointer shadow"
+          className="w-10 h-10 flex items-center justify-center bg-linear-to-r from-violet-500/85 to-violet-900/95 dark:from-violet-950 dark:to-slate-900 rounded-full cursor-pointer shadow"
         >
-          <img src={profileImage} alt="Profile icon" className="w-10" />
+          <img src={profileImage} alt="Profile icon" className="w-7" />
         </Link>
       </div>
     </header>
