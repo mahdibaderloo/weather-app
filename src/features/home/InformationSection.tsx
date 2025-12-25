8d76b2b8878311d442b406cd99241f83fdeb26f0
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-12-25 12:34:42 +0330

    fix Home(first box) styles for laptop size

--- src/features/home/InformationSection.tsx
+++ src/features/home/InformationSection.tsx
@@ -40,48 +40,49 @@ export default function InformationSection({
   return (
     <section className="lg:relative lg:z-50 p-4 bg-linear-to-r from-violet-800/60 to-violet-400/35 dark:from-slate-950/95 dark:to-slate-950/50 rounded-4xl w-full lg:w-[49%] h-fit flex flex-col md:flex-row justify-between mt-4 lg:mt-0">
       {/* <div className="absolute blur-sm w-full h-full" /> */}
-      <div className="flex flex-col lg:gap-6 2xl:gap-10">
-        <div className="w-full lg:w-fit flex items-center gap-2 bg-violet-950 dark:bg-linear-to-l dark:from-slate-950/95 dark:to-slate-950/30 rounded-2xl pr-6 pl-1 py-1">
-          <img src={locationIcon} alt="Location icon" className="w-8" />
-          <p className="font-semibold text-lg text-violet-200 w-fit truncate">
+      <div className="flex flex-col lg:gap-4 2xl:gap-10">
+        <div className="w-full lg:w-fit  flex items-center gap-2 lg:gap-1 bg-violet-950 dark:bg-linear-to-l dark:from-slate-950/95 dark:to-slate-950/30 rounded-2xl pr-6 lg:pr-2 pl-1 py-1">
+          <img src={locationIcon} alt="Location icon" className="w-8 lg:w-5" />
+          <p className="font-semibold text-lg lg:text-[16px] text-violet-200 w-fit truncate">
             {city || "City"}
           </p>
         </div>
+
         <div className="flex flex-col items-center mt-6 md:hidden">
           <img src={icon} alt="Icon" className="w-40" />
           <p className="font-semibold text-2xl text-violet-950">{weather}</p>
         </div>
 
-        <div>
-          <p className="font-bold text-3xl xl:text-4xl 2xl:text-5xl text-violet-100 mt-10 md:mt-9 lg:mt-0">
+        <div className="lg:flex lg:flex-col">
+          <p className="font-bold text-3xl lg:text-xl xl:text-4xl 2xl:text-5xl text-violet-100 mt-10 md:mt-9 lg:mt-0">
             {getWeekday()}
           </p>
-          <span className="xl:text-lg font-semibold text-violet-300">
+          <span className="lg:text-[16px] xl:text-lg font-semibold text-violet-300">
             {getFormattedDate(selectedDate)}
           </span>
         </div>
 
-        <div>
-          <p className="font-bold text-4xl xl:text-5xl 2xl:text-6xl text-violet-100 mt-6 lg:mt-0">
+        <div className="lg:flex lg:flex-col">
+          <p className="font-bold text-4xl lg:text-2xl xl:text-5xl 2xl:text-6xl text-violet-100 mt-6 lg:mt-0">
             {Math.floor(temp)}°{unit}
           </p>
-          <p className="xl:text-lg font-semibold text-violet-300">
+          <p className="lg:text-[16px] xl:text-lg font-semibold text-violet-300">
             High: {Math.floor(maxTemp)} Low: {Math.floor(minTemp)}
           </p>
         </div>
 
-        <p className="xl:text-lg font-semibold text-violet-200 mt-6 lg:mt-0">
+        <p className="lg:text-[16px] xl:text-lg font-semibold text-violet-200 mt-6 lg:mt-0">
           Wind Speed: {windSpeed} {windUnit}
         </p>
       </div>
 
-      <div className="flex flex-col justify-between items-end md:gap-10 xl:gap-6">
+      <div className="flex flex-col justify-between items-end md:gap-10 lg:gap-4 xl:gap-6">
         <div
           className="hidden w-fit md:flex items-center justify-between bg-violet-200/40 rounded-full cursor-pointer"
           onClick={handleToggleUnit}
         >
           <span
-            className={`w-8 h-8 rounded-full font-medium flex justify-center items-center transition-all ${
+            className={`w-8 h-8 lg:w-6 lg:h-6 rounded-full font-medium lg:text-sm flex justify-center items-center transition-all ${
               unit === "F"
                 ? "bg-violet-950 text-violet-200 dark:bg-linear-to-r dark:from-slate-950/95 dark:to-slate-950/30"
                 : "text-violet-950 dark:text-slate-950"
@@ -90,7 +91,7 @@ export default function InformationSection({
             F
           </span>
           <span
-            className={`w-8 h-8 rounded-full font-medium flex justify-center items-center transition-all ${
+            className={`w-8 h-8 lg:w-6 lg:h-6 rounded-full font-medium lg:text-sm flex justify-center items-center transition-all ${
               unit === "C"
                 ? "bg-violet-950 text-violet-200 dark:bg-linear-to-r dark:from-slate-950/95 dark:to-slate-950/30"
                 : "text-violet-950 dark:text-slate-950"
@@ -101,8 +102,8 @@ export default function InformationSection({
         </div>
 
         <div className="hidden md:flex flex-col items-end lg:mb-0">
-          <img src={icon} alt="Icon" className="md:w-40 xl:w-50" />
-          <p className="font-semibold text-2xl xl:text-3xl text-violet-950 lg:mt-0">
+          <img src={icon} alt="Icon" className="md:w-40 lg:w-24 xl:w-50" />
+          <p className="font-semibold text-2xl lg:text-lg xl:text-3xl text-violet-950 lg:mt-2">
             {weather}
           </p>
         </div>
