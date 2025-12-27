213ab7f791031067411320dc39b8b732951ec278
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-12-27 20:26:23 +0330

    fix max width for large size

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -6,7 +6,7 @@ import MobileNavbar from "./components/MobileNavbar";
 export default function AppLayout() {
   return (
     <div className="bg-linear-to-r from-violet-400 to-violet-300/85 dark:from-slate-900 dark:to-slate-950 h-screen overflow-scroll xl:overflow-hidden">
-      <div className="flex gap-10 h-full 2xl:max-w-7xl 2xl:mx-auto">
+      <div className="flex gap-10 h-full 2xl:max-w-6xl 2xl:mx-auto">
         <Navbar />
         <MobileNavbar />
         <div className="h-full w-full flex flex-col gap-8 px-4 lg:px-0">
