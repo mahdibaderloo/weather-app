4e62c17ac9cc2bc180594ccdb1617d64ce35658e
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-12-27 20:16:44 +0330

    fix Home (second box) styles for desktop size

--- src/features/home/DailyItem.tsx
+++ src/features/home/DailyItem.tsx
@@ -22,21 +22,21 @@ export default function DailyItem({ data, day }: DailyProps) {
   const minTemp = useToggleUnitValue(data.temperature_2m_min[day]);
 
   return (
-    <li className="flex items-center justify-between text-violet-100 lg:text-violet-300 dark:text-slate-400 lg:text-xl font-semibold py-4 lg:py-3 xl:py-3 2xl:py-4">
+    <li className="flex items-center justify-between text-violet-100 lg:text-violet-300 dark:text-slate-400 lg:text-xl font-semibold py-4 lg:py-3 xl:py-2 2xl:py-4">
       <p className="w-20 lg:text-sm">{getNextDay(day)}</p>
 
       <div className="flex items-center">
         <img
           src={icon}
           alt="icon"
-          className="w-10 lg:w-7 xl:w-14 2xl:w-16 mr-2"
+          className="w-10 lg:w-7 xl:w-10 2xl:w-16 mr-2"
         />
         <span className="dark:text-violet-800 xl:text-shadow-[0_0_2px] lg:text-sm text-shadow-violet-700">
           {weatherName === "Partly Cloudy" ? "Cloudy" : weatherName}
         </span>
       </div>
 
-      <p className="lg:text-[16px]">
+      <p className="lg:text-[16px] xl:text-sm">
         {Math.floor(maxTemp)} /{" "}
         <span className="text-violet-700 lg:text-violet-400 dark:text-slate-600">
           {Math.floor(minTemp)}
